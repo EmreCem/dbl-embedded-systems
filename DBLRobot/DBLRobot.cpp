21a7b6878e0d664d6a57541a65e8249365441889
Hoogerheijde <r.hoogerheijde@student.tue.nl>
2020-04-08 15:45:09 +0200

    Added DiskBinary method

--- DBLRobot/DBLRobot.cpp
+++ DBLRobot/DBLRobot.cpp
@@ -3,8 +3,36 @@
 #include <iostream>
 #include "ILogger.h"
 #include "ConsoleLogger.h"
+//#include "MotorController.h"
+//#include "SensorReader.h"
+
+std::string DiskBinary(int Num) // Returns string with disk colors needed to make the number
+{
+    std::string DiskString;
+
+    if (Num == 0)
+    {
+        return "b";
+    }
+
+    while (Num != 0) {
+        if (Num % 2 == 0) {
+            DiskString = 'b' + DiskString;
+        }
+        else {
+            DiskString = 'w' + DiskString;
+        }
+        Num /= 2; 
+    }
+
+    return DiskString;
+}
 
 int main() {
-    wiringPiSetupGpio();
-    ConsoleLogger logger{};
+    int Num = 0;
+    for (int i = 0; i <= 7; i++) {
+        std::cout << i << "\t" << DiskBinary(i) << "\n";
+    }
+    //wiringPiSetupGpio();
+    //ConsoleLogger logger{};
 }
