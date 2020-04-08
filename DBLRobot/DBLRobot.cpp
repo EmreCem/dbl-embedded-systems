05f5c0626f62d2648a5bcf700ac3e2dab02a7ea8
Hoogerheijde <r.hoogerheijde@student.tue.nl>
2020-04-08 16:26:29 +0200

    Started working on logic

--- DBLRobot/DBLRobot.cpp
+++ DBLRobot/DBLRobot.cpp
@@ -30,9 +30,26 @@ std::string DiskBinary(int Num) // Returns string with disk colors needed to mak
 
 int main() {
     int Num = 0;
-    for (int i = 0; i <= 7; i++) {
-        std::cout << i << "\t" << DiskBinary(i) << "\n";
+    std::string DiskString;
+    char CurrDisks[7];
+    char NeededDisk;
+    int RampPos = 4; // The current position under the ramp on the tape
+    int SensorPos = 6; // The current position under the rgb sensor on the tape. 
+    while (Num <= 7) {
+        DiskString = DiskBinary(Num);
+        std::cout << Num << "\t" << DiskString << "\n";
+        while (!DiskString.empty()) {
+            NeededDisk = DiskString.back(); // Disk needed to form binary number
+            // Check for Needed Disk on conveyor belt
+            // If found push on tape
+            // Save value in currDisk at currPos - 
+            std::cout << "\t" << NeededDisk << "\n";
+            DiskString.pop_back(); // Remove found disk from string
+        }
+        Num++;
     }
+
+    
     //wiringPiSetupGpio();
     //ConsoleLogger logger{};
 }
